5c85f0d6d0aac76647a6cc2ffd42804a7aa18401
Marlon <marlon_scholten@live.nl>
2020-02-21 11:50:06 +0100

    deels methodes geschreven voor exemplaar en verhuurproduct classes

--- praxa/src/nl/rubend/Exemplaar.java
+++ praxa/src/nl/rubend/Exemplaar.java
@@ -8,4 +8,21 @@ public class Exemplaar {
 	private int serieNr;
 	private String status;
 	private ArrayList<VerhuurRegel> regels=new ArrayList<VerhuurRegel>();
+
+	public Exemplaar(){
+
+	}
+
+	public Boolean isVerhuurd(){
+		Boolean verhuurd = null;
+
+		if (status.equals("Verhuurd")){
+			verhuurd = true;
+		} else if (status.equals("Beschikbaar")){
+			verhuurd = false;
+		} else if (status.equals("") || status.equals(null)) {
+				System.out.println("Status niet beschikbaar");
+		}
+		return verhuurd;
+	}
 }
--- praxa/src/nl/rubend/VerhuurProduct.java
+++ praxa/src/nl/rubend/VerhuurProduct.java
@@ -3,8 +3,28 @@ package nl.rubend;
 import java.util.ArrayList;
 
 public class VerhuurProduct extends Product {
-	private static ArrayList<VerhuurProduct> alleVerhuurProducten;
+	private static ArrayList<VerhuurProduct> alleVerhuurProducten = new ArrayList<VerhuurProduct>();
 	private double verhuurPrijs;
 	private double borg;
-	private ArrayList<Exemplaar> exemplaren=new ArrayList<Exemplaar>();
+	private ArrayList<Exemplaar> exemplaren = new ArrayList<Exemplaar>();
+
+	public VerhuurProduct(){
+
+	}
+
+	public static ArrayList geefAlle(){
+		for (VerhuurProduct VerhuurProduct : alleVerhuurProducten) {
+			alleVerhuurProducten.add(VerhuurProduct);
+		}
+		return alleVerhuurProducten;
+	}
+
+	public ArrayList geefBeschikbareExemplaren(){
+		for(Exemplaar Exemplaar: exemplaren){
+			if (Exemplaar.isVerhuurd() == false){
+				exemplaren.add(Exemplaar);
+			}
+		}
+		return exemplaren;
+	}
 }
