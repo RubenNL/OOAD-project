5d38e150fe9f7fc49943578cd506c3bb6afcdd96
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-02-21 11:31:57 +0100

    paar aanpassingen...

--- praxa/src/nl/rubend/VerhuurTransactie.java
+++ praxa/src/nl/rubend/VerhuurTransactie.java
@@ -3,4 +3,8 @@ package nl.rubend;
 public class VerhuurTransactie extends Transactie {
 	private double borgTotaal;
 	private double afrekenTotaal;
+	public VerhuurTransactie() {
+		borgTotaal=0;
+		afrekenTotaal=0;
+	}
 }
