b579ea73b2cba2a6179461fed432190e2ddd4d00
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-02-21 10:45:21 +0100

    ArrayList init

--- praxa/src/nl/rubend/VerhuurProduct.java
+++ praxa/src/nl/rubend/VerhuurProduct.java
@@ -3,7 +3,7 @@ package nl.rubend;
 import java.util.ArrayList;
 
 public class VerhuurProduct extends Product {
-	private static ArrayList<VerhuurProduct> alleVerhuurProducten;
+	private static ArrayList<VerhuurProduct> alleVerhuurProducten=new ArrayList<VerhuurProduct>();
 	private double verhuurPrijs;
 	private double borg;
 	private ArrayList<Exemplaar> exemplaren=new ArrayList<Exemplaar>();
