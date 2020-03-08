4ef1a9f9e11c0f251a8965ca716668be931c5c96
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-03-08 12:12:53 +0100

    Deze functie moet nogwel getest worden

--- praxa/src/nl/rubend/VerhuurRegel.java
+++ praxa/src/nl/rubend/VerhuurRegel.java
@@ -2,22 +2,22 @@ package nl.rubend;
 
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class VerhuurRegel extends TransactieRegel {
 	private Date eindDatum;
-	private int aantalDagen;
-
-	public VerhuurRegel(int aantal, double regTot,Date eindDatum) {
+	private Date startDatum;
+	public VerhuurRegel(int aantal, double regTot,Date startDatum, Date eindDatum) {
 		super(aantal, regTot);
+		this.startDatum=startDatum;
 		this.eindDatum=eindDatum;
 	}
-
+	public int getAantalDagen() {
+		long diff = eindDatum.getTime() - startDatum.getTime();
+		return (int) TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
 	public Date geefDatum() {
 		return this.eindDatum;
-	}
-
-	public int getAantalDagen() {
-		return this.aantalDagen;
 	}
 
 }
