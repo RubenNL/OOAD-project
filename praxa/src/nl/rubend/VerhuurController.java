56fea2f6039af777b83e0343e76defe549766657
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-03-09 11:32:20 +0100

    start functie miste nog

--- praxa/src/nl/rubend/VerhuurController.java
+++ praxa/src/nl/rubend/VerhuurController.java
@@ -3,6 +3,9 @@ package nl.rubend;
 import java.util.ArrayList;
 
 public class VerhuurController {
+	public ArrayList<Product> start() {
+		return VerhuurProduct.geefAlle();
+	}
 	public Product product;
 	public Exemplaar exemplaar;
 }
