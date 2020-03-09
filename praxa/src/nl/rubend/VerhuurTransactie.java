d18f47a93e1f89e7274f8b2cbc678aff4a7e1847
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-03-09 12:10:57 +0100

    nog niet af!

--- praxa/src/nl/rubend/VerhuurTransactie.java
+++ praxa/src/nl/rubend/VerhuurTransactie.java
@@ -1,10 +1,11 @@
 package nl.rubend;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class VerhuurTransactie extends Transactie {
 	private double borgTotaal;
-
+	private ArrayList<VerhuurRegel> verhuurRegels=new ArrayList<VerhuurRegel>();
 //	een afgeleide attribuut
 //	private double afrekenTotaal;
 
@@ -15,5 +16,8 @@ public class VerhuurTransactie extends Transactie {
 	public VerhuurTransactie(Date datum, String tijd, String plaats) {
 		super("verhuur", datum, tijd, plaats);
 		this.borgTotaal=0;
+	}
+	public void voegVerhuurRegelToe(VerhuurRegel verhuurRegel) {
+		verhuurRegels.add(new verhuurRegel());
 	}
 }
