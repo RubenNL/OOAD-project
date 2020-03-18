c3823966b076e1947fdc6eb148c389d73ab865c5
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-03-18 09:55:38 +0100

    layout van BoekVlucht staat nu in een gridpane, makkelijker te bewerken

--- LuchtvaartMaatschappijSysteem/src/main/userInterfaceLaag/BoekVluchtController.java
+++ LuchtvaartMaatschappijSysteem/src/main/userInterfaceLaag/BoekVluchtController.java
@@ -1,4 +1,27 @@
 package main.userInterfaceLaag;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Spinner;
+
 public class BoekVluchtController {
+
+	@FXML
+	private ComboBox<?> vetrekVliegveld;
+
+	@FXML
+	private ComboBox<?> aankomstVliegveld;
+
+	@FXML
+	private DatePicker vertrekDatum;
+
+	@FXML
+	private ComboBox<?> vertrekTijd;
+
+	@FXML
+	private ComboBox<?> klasse;
+
+	@FXML
+	private Spinner stoelen;
 }
