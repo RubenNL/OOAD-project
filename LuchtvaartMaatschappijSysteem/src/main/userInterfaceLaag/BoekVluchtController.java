66174f448e2828ef55cd81524164aacb3446b508
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-03-18 11:03:35 +0100

    vertrekVliegveld werkt... soort van

--- LuchtvaartMaatschappijSysteem/src/main/userInterfaceLaag/BoekVluchtController.java
+++ LuchtvaartMaatschappijSysteem/src/main/userInterfaceLaag/BoekVluchtController.java
@@ -1,14 +1,23 @@
 package main.userInterfaceLaag;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Spinner;
+import main.domeinLaag.Land;
+import main.domeinLaag.Luchthaven;
 
-public class BoekVluchtController {
+import java.net.URL;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+public class BoekVluchtController implements Initializable {
 
 	@FXML
-	private ComboBox<?> vetrekVliegveld;
+	private ComboBox<String> vertrekVliegveld;
 
 	@FXML
 	private ComboBox<?> aankomstVliegveld;
@@ -24,4 +33,9 @@ public class BoekVluchtController {
 
 	@FXML
 	private Spinner stoelen;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		vertrekVliegveld.getItems().setAll(Luchthaven.geefAlle().keySet());
+	}
 }
